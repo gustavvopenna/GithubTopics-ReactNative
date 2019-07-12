9172b8df8d0e0fd334674e570623178770a59d0d
gustavvopenna <gustavo.pema@gmail.com>
2019-07-12 03:40:17 -0500

    Create TopBar at Home Screen

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { Text } from 'react-native'
 import styled from 'styled-components'
+
+import { NotificationIcon } from './components/Icons'
 
 export default function App() {
   return (
     <Container>
-      <Text>Hello World heeey!</Text>
+      <TitleBar>
+        <Avatar source={require('./assets/avatar.jpg')} />
+        <Title>Welcome back,</Title>
+        <Name>Gustavo</Name>
+        <NotificationIcon
+          style={{ position: 'absolute', right: 20, top: 5, zIndex: 1 }}
+        />
+      </TitleBar>
     </Container>
   )
 }
@@ -13,6 +21,31 @@ export default function App() {
 const Container = styled.View`
   flex: 1;
   background: #f0f3f5;
-  align-items: center;
-  justify-content: center;
+`
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  border-radius: 22px;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  margin-left: 20px;
+`
+
+const Title = styled.Text`
+  font-size: 16px;
+  font-weight: 500;
+  color: #b8bece;
+`
+
+const Name = styled.Text`
+  font-size: 22px;
+  font-weight: bold;
+  color: #3c4560;
 `
